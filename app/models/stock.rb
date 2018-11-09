d61c62246a8503cce1c8b0dc958e24791f618f78
vaneEmy <vanvanemily@gmail.com>
2018-11-09 00:53:57 -0600

    Generating a Stock model and define a class method which return a stock instance.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+    def self.new_from_lookup(ticker_symbol)
+        looked_up_stocK = StockQuote::Stock.quote(ticker_symbol)
+        new(name: looked_up_stocK.company_name, ticker: looked_up_stocK.symbol, price: looked_up_stocK.open)
+    end
 end
