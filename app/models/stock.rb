95d32746980ac1fe6666b7391aa1470ae94f1d7f
Vanessa <vanvanemily@gmail.com>
2019-01-07 22:56:52 -0600

    Validating if the symbol is not nil

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 class Stock < ApplicationRecord
     def self.new_from_lookup(ticker_symbol)
-        looked_up_stocK = StockQuote::Stock.quote(ticker_symbol)
-        new(name: looked_up_stocK.company_name, ticker: looked_up_stocK.symbol, price: looked_up_stocK.open)
+        begin
+            looked_up_stocK = StockQuote::Stock.quote(ticker_symbol)
+            new(name: looked_up_stocK.company_name, ticker: looked_up_stocK.symbol, price: looked_up_stocK.calculation_price)
+        rescue Exception => e
+            return nil
+        end
     end
 end
