ac9a1c1eb86f509f020de2e2921278cfeb235b51
Vanessa <vanvanemily@gmail.com>
2019-01-12 02:01:24 -0600

    Adding Ajax for searching finance symbols

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require rails-ujs
 //= require jquery
 //= require twitter/bootstrap
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+    $('#stock-lookup-form').on('ajax:complete', function(event, data, status){
+        $('#results').html(data.responseText)
+    })
+  })
